ca111d1c5d888da21dc050280175a173738c8ef6
Memet <meavci@gmail.com>
2021-01-10 18:33:45 -0500

    Create an array for blog post

--- Desktop/cemil hoca ders/my-block/src/App.js
+++ Desktop/cemil hoca ders/my-block/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 
-
+const posts=[
+  {
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",},
+  {title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+  body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"},
+  {title: "qui est esse",
+  body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"},
+  {title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+  body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"},
+  {title: "eum et est occaecati",
+  body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"}
+ ]
 
 function App() {
   return (
     <div>
       <h1 className="text-center">Welcome to my Blog</h1>
       <section>
+        {posts.map((globe)=>{
+         return( <article>
+<h2>{globe.title}</h2>
+<p>{globe.body}</p>
+</article>)
+
+        })}
       <article>
       <h2>Blog Post Title 1</h2>
      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
