a472903991d09edec8a8dcba602f98fb37ab4820
Memet <meavci@gmail.com>
2021-01-13 23:15:47 -0500

    create post for seperate page

--- Desktop/cemil hoca ders/my-block/src/components/Posts.js
+++ Desktop/cemil hoca ders/my-block/src/components/Posts.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function Posts(props){
   return( <div>
  <section>
-        {props.blogPosts.map((globe)=>{
-         return( <article>
-<h2>{globe.title}</h2>
-<p>{globe.body}</p>
+        {props.blogPosts.map((globe,index)=>{
+         return( <article key={index}>
+           <Link to={'/posts/'+ globe.id }><h2>{globe.title}</h2></Link>
+{/* <p>{globe.body}</p> */}
 </article>)
       })}
       </section>
