86e07030fea3fc5857d888063096a87358221f7f
Memet <meavci@gmail.com>
2021-01-31 17:32:32 -0500

    create action and reducer

---
+++ Desktop/react ders/click-app-redux/reduxcounter/src/actions/counterAction.js
@@ -0,0 +1,8 @@
+const INCREASE_COUNT= 'INCREASE_COUNT';
+const DECREASE_COUNT= 'DECREASE_COUNT';
+export const increaseCount=()=>({
+  type:INCREASE_COUNT
+} )
+export const decreaseCount=()=>({
+  type:DECREASE_COUNT
+} )
